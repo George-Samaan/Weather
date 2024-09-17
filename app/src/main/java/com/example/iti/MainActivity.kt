4cf03d1a7204912ccabc48cd56da8b414ec3db0e
George-Samaan <georgemichel07@icloud.com>
2024-09-17 22:02:19 +0300

    fixing

--- app/src/main/java/com/example/iti/MainActivity.kt
+++ app/src/main/java/com/example/iti/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.iti
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.iti.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnMaps.setOnClickListener {
+            val intent = Intent(this, GoogleMapsActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnSettings.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
