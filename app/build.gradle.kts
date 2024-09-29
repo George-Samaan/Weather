58df52830dccf2f00e58a875c29072cfa155c09d
George-Samaan <georgemichel07@icloud.com>
2024-09-30 00:32:13 +0300

    - add debug apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.iti"
-        minSdk = 31
+        minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
